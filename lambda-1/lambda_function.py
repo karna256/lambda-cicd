b3302d6081d7da81b7d7bb7234fd016a923b1c02
karna256 <ankitkarna@lftechnology.com>
2024-05-09 14:42:02 +0545

    chore: update lambda1

--- lambda-1/lambda_function.py
+++ lambda-1/lambda_function.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from lambda-1')
+        'body': json.dumps('Hello from lambda-123')
     }
